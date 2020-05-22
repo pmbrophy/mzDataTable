4c2946c622543ffe89a469d36025832ab327f548
unknown <unknown@USAMIL-R90WW32X.corp.waters.com>
2020-05-21 20:49:26 -0400

    Functioning Rcpp, still need to add names to list

--- src/C_normCentroidMz.cpp
+++ src/C_normCentroidMz.cpp
@@ -1,29 +1,50 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-// This is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp
-// function (or via the Source button on the editor toolbar). Learn
-// more about Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//   http://gallery.rcpp.org/
-//
 
+
+//' Rcpp function for normCentroidMz
+//' SEXP
+//' @param mz_vector A sorted numeric vecotor
+//' @param mz_upperLimit_vector the mz_vector multiplied by ppmTol*(10^(-6)) and added to mz
+//'
+//' @export
 // [[Rcpp::export]]
-NumericVector C_normCentroidMz(NumericVector mz) {
+List C_normCentroidMz(SEXP mz_vector, SEXP mz_upperLimit_vector) {
+  //translate inputs from R to C++
+  Rcpp::NumericVector mz(mz_vector);
+  Rcpp::NumericVector mz_upperLimit(mz_upperLimit_vector);
+
   int mz_length = mz.size();
 
-  return mz_length
+  //generate new NumericVectors
+  NumericVector mzGrid (mz_length);
+  NumericVector mzGrid_index (mz_length);
+  NumericVector mzGrid_upperLimit (mz_length);
+
+  //set value of first element in each vecotr
+  mzGrid[0] = mz[0];
+  mzGrid_upperLimit[0] = mz_upperLimit[0];
+
+  //loop
+  for(int i = 1; i < mz_length; i++) {
+    if(mzGrid_upperLimit[i-1] > mz[i]) {
+      //Current grid value is same as previous
+      mzGrid[i] = mzGrid[i-1];
+      //Current index is same as previous
+      mzGrid_index[i] = mzGrid_index[i-1];
+      //Current upper limit is same as previous
+      mzGrid_upperLimit[i] = mzGrid_upperLimit[i-1];
+    }
+    else {
+      //Current grid value new mz
+      mzGrid[i] = mz[i];
+      //Current index is same as previous
+      mzGrid_index[i] = mzGrid_index[i-1] + 1;
+      //Current upper limit is updated
+      mzGrid_upperLimit[i] = mz_upperLimit[i];
+    }
+  }
+
+  return List::create(mzGrid, mzGrid_index, mzGrid_upperLimit);
 }
-
-
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
