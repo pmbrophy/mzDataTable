c1fb5ee0d74107775dc7acd2596f02fa6fb583fb
unknown <unknown@USAMIL-R90WW32X.corp.waters.com>
2020-05-27 15:20:11 -0400

    Formatting

--- R/getXIC_dt.R
+++ R/getXIC_dt.R
@@ -7,7 +7,7 @@
 #' @param mz the m/z value to extract.
 #' @param mz_delta Optional. A numeric value +/- the mz value to extract.
 #'   Default = NULL.
-#' @param massAccuracy Optional. The mass accuracy of the instrument in ppm.
+#' @param ppmTol Optional. The mass accuracy of the instrument in ppm.
 #'   Default = NULL
 #' @param iStart Optional. The integer start index to begin extracting ions.
 #'   Default = NULL.
@@ -19,12 +19,12 @@
 #'   Default = NULL.
 #'
 #' @details mzDt and mz must be provided. Default behavior is to extract ions
-#'   exactly matching mz value provided accross all time.
+#'   exactly matching mz value accross all time.
 #'
-#'   Specifying mz_delta or massAccuracy will result in mz filtering. mz_delta
+#'   Specifying mz_delta or ppmTol will result in mz filtering. mz_delta
 #'   will extract ions with values in the range where mz >= mz-mz_delta and mz
-#'   <= mz+mz_delta. Specifying massAccuracy workes similarly, but calculates
-#'   the upper and lower bounds from ppm error.
+#'   <= mz+mz_delta. Specifying ppmTol workes similarly, but calculates
+#'   the upper and lower bounds from ppm mass error.
 #'
 #'   Specifying iStart/iStop or tStart/tStop will result in time filtering.
 #'
@@ -33,21 +33,22 @@
 #'
 #' @examples
 #'
-getXIC_dt <- function(mzDt, mz, mz_delta = NULL, massAccuracy = NULL, iStart = NULL, iStop = NULL, tStart = NULL, tStop = NULL){
+
+getXIC_dt <- function(mzDt, mz, mz_delta = NULL, ppmTol = NULL, iStart = NULL, iStop = NULL, tStart = NULL, tStop = NULL){
   #Check data.table
   .check_mzDataTable(mzDt)
 
   #mz Filter
-  if(!is.null(mz_delta) & !is.null(massAccuracy)){
-    stop("mz_delta and massAccuracy provided. Set only one for m/z filter or none for exact matching.")
+  if(!is.null(mz_delta) & !is.null(ppmTol)){
+    stop("mz_delta and ppmTol provided. Set only one for m/z filter or none for exact matching.")
   }else if(!is.null(mz_delta)){
     #Filter using mz_delta
     mz_min <- mz - mz_delta
     mz_max <- mz + mz_delta
-  }else if(!is.null(massAccuracy)){
-    #Filter using massAccuracy
-    mz_min <- mz - mz*(1E-6)*massAccuracy
-    mz_max <- mz + mz*(1E-6)*massAccuracy
+  }else if(!is.null(ppmTol)){
+    #Filter using ppmTol
+    mz_min <- mz - mz*(1E-6)*ppmTol
+    mz_max <- mz + mz*(1E-6)*ppmTol
   }else{
     mz_min <- mz
     mz_max <- mz
@@ -57,6 +58,10 @@ getXIC_dt <- function(mzDt, mz, mz_delta = NULL, massAccuracy = NULL, iStart = N
   if((!is.null(iStart) | !is.null(iStop)) & (!is.null(tStart) | !is.null(tStop))){
     stop("iStart/iStop and tStart/tStop cannot both be specified")
   }else if(!is.null(iStart) | !is.null(iStop)){
+    ####################################################################################### #
+    ###########################       TIME FILTER BY SeqNum           ##################### #
+    ####################################################################################### #
+
     #Fill Missing Values
     if(is.null(iStart)){
       iStart <- min(mzDt$seqNum)
@@ -72,6 +77,10 @@ getXIC_dt <- function(mzDt, mz, mz_delta = NULL, massAccuracy = NULL, iStart = N
          by = list(seqNum, retentionTime)]
 
   }else if(!is.null(tStart) | !is.null(tStop)){
+    ####################################################################################### #
+    #######################       TIME FILTER BY retentionTime           ################## #
+    ####################################################################################### #
+
     #Fill Missing Values
     if(is.null(tStart)){
       tStart <- min(mzDt$retentionTime)
@@ -87,6 +96,10 @@ getXIC_dt <- function(mzDt, mz, mz_delta = NULL, massAccuracy = NULL, iStart = N
          by = list(seqNum, retentionTime)]
 
   }else{
+    ####################################################################################### #
+    ##############################       NO TIME FILTER           ######################### #
+    ####################################################################################### #
+
     mzDt[mz %between% c(mz_min, mz_max),
          list(sumIntensity = sum(intensity),
               mzMax = max(mz),
