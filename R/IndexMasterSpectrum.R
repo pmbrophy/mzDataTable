20203f7fbe122ec57b77ec983495a6c488d10c77
unknown <unknown@USAMIL-R90WW32X.corp.waters.com>
2020-05-29 07:48:13 -0400

    Update input parameter name

--- R/IndexMasterSpectrum.R
+++ R/IndexMasterSpectrum.R
@@ -3,7 +3,7 @@
 #' @description Index the m/z axis for a set of mass spectra. Function outputs
 #'   normalized m/z values and integer index values.
 #'
-#' @param dt a data.table containing a column named mz
+#' @param mzDt a data.table containing a column named mz
 #' @param ppmTol the grouping tolerance in ppm
 #' @param isCentroid Logical. The default is `TRUE`. Are the spectra centroids?
 #'
@@ -15,29 +15,30 @@
 #' mz <- c(100, 100.01, 100.02, 100.021, 100.2, 100.21, 100.22, 100.22, 100.31)
 #'
 
-indexMasterSpectrum <- function(dt, ppmTol, isCentroid = TRUE){
+indexMasterSpectrum <- function(mzDt, ppmTol, isCentroid = TRUE){
   #Input checks
-  notDataTable <- !data.table::is.data.table(dt)
-  mzColMissing <- !("mz" %in% colnames(dt))
+  notDataTable <- !data.table::is.data.table(mzDt)
+  mzColMissing <- !("mz" %in% colnames(mzDt))
+
   if(notDataTable){
-    stop("dt is not a data.table")
+    stop("mzDt is not a data.table")
   }else if(mzColMissing){
-    stop("dt does not contain a column named 'mz'")
+    stop("mzDt does not contain a column named 'mz'")
   }
 
   #Do the Gridding: Both return at least:
   # "mzGrid" - the m/z values of each grid and
   # "mzGrid_index" - an index value for each grid.
   if(isCentroid){
-    dt_grid <- .C_normCentroidMz(mz = dt$mz, ppmTol = ppmTol)
-    dt_grid[, mz := NULL]
+    mzDt_grid <- .C_normCentroidMz(mz = mzDt$mz, ppmTol = ppmTol)
+    mzDt_grid[, mz := NULL]
   }else{
-    dt_grid <- .normProfileMz(mz = dt$mz)
+    mzDt_grid <- .normProfileMz(mz = mzDt$mz)
   }
 
   #Return result
-  dt[, c("mzGrid", "mzGrid_index") := list(dt_grid$mzGrid, dt_grid$mzGrid_index)]
-  dt
+  mzDt[, c("mzGrid", "mzGrid_index") := list(mzDt_grid$mzGrid, mzDt_grid$mzGrid_index)]
+  mzDt
 }
 
 #' Calculate global m/z grid for profile mode data
